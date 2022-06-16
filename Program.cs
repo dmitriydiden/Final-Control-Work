62cbcd2530cccc27c1d842743403489270bf61bf
dmitry_di <dnd_d@mail.ru>
2022-06-16 11:08:31 +0300

    написали программу создания массива из строк

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+Console.Clear();
+Console.Write("Введите желаемое количество строк в массиве:");
+int l=int.Parse(Console.ReadLine());
+string[] array = new string[l];
+int count = 0;
+string[] newarray;
+
+Console.WriteLine($"Введите строки массива:");
+for (int i = 0; i < l; i++)
+{
+    array[i] = Console.ReadLine();
+    if (array[i].Length < 4)
+    {
+        count++;
+    }
+}
+newarray = new string[count];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    {
+        newarray[j] = array[i];
+        j++;
+    }
+}
+Console.Write("[{0}]", string.Join(", ", array));
+Console.Write(" -> [{0}]", string.Join(", ", newarray));
