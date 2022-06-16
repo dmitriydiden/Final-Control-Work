ef90d35e319908d104e84b0361846d306aed40fd
dmitry_di <dnd_d@mail.ru>
2022-06-16 12:04:11 +0300

    Вариант 2. Написали программу формирование нового массива из строк длиной 3 и менее символов с использованием метода

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите желаемое количество строк в массиве:");
+int l=int.Parse(Console.ReadLine());
+string[] array = new string[l];
+int count = 0;
+
+Console.WriteLine($"Введите строки массива:");
+for (int i = 0; i < l; i++)
+{
+    array[i] = Console.ReadLine();
+    if (array[i].Length < 4)
+    {
+        count++;
+    }
+}
+string[] newarray=NewArray(array, count);
+
+string[] NewArray(string[] arr, int rows)
+{
+    string[] newarr = new string[rows];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 4)
+        {
+            newarr[j] = arr[i];
+            j++;
+        }
+    }
+    return newarr;
+}
+
+Console.Write("[{0}]", string.Join(", ", array));
+Console.Write(" -> [{0}]", string.Join(", ", newarray));
+
